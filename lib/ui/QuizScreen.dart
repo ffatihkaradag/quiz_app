004c10abf1383c5c8ba977f628516c5b7913b537
ffatihkaradag <ffatihkaradag@gmail.com>
2021-01-28 02:15:44 +0300

    initial commit

---
+++ lib/ui/QuizScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quiz_app/controllers/question_controller.dart';
+
+import 'QzBody.dart';
+
+
+class QuizScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    QuestionController _controller = Get.put(QuestionController());
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          FlatButton(onPressed: _controller.nextQuestion, child: Text("Skip")),
+        ],
+      ),
+      body: QzBody(),
+    );
+  }
+
+}
